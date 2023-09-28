5bf076a8ae842811ad69281d621646cde79a7d56
mohamedkhedr700 <M.khedr@rowaad.net>
2023-09-29 01:23:04 +0300

    First

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -3,12 +3,10 @@
 namespace Raid\Core\Command\Traits;
 
 use Raid\Core\Command\Traits\Command\WithCommandHelper;
-use Raid\Core\Command\Traits\Command\WithCreateCommand;
 use Illuminate\Console\Command as IlluminateCommand;
 class Command extends IlluminateCommand
 {
-    use WithCommandHelper,
-        WithCreateCommand;
+    use WithCommandHelper;
 
     /**
      * The name and signature of the console command.
---
+++ src/Commands/CreateCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Raid\Core\Command\Traits;
+
+use Raid\Core\Command\Traits\Command\WithCreateCommand;
+
+class CreateCommand extends Command
+{
+    use WithCreateCommand;
+}
